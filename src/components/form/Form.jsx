81c8703835bc82066efab7f354df3b0df163fab2
ljs981026 <ljs981026@naver.com>
2025-01-14 22:48:47 +0900

    로그인, 회원가입 기능 구현

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import styles from './Form.module.scss';
 import { useForm } from 'react-hook-form';
 
-const Form = ({title}) => {
+const Form = ({title, getDataForm, firebaseError}) => {
   const { register, handleSubmit, formState: {errors}, reset } = useForm({
     mode: 'onChange'
   })
   
-  const onSubmit = ({ email, password }) => {
-    console.log(email, password)
+  const onSubmit = ({ email, password }) => {    
+    getDataForm(email, password)
+    reset();
   }
 
   const userEmail = {
@@ -22,8 +23,8 @@ const Form = ({title}) => {
   const userPassword = {
     required: "필수 입력값입니다.",
     minLength: {
-      value: 4,
-      message: "최소 4자입니다."
+      value: 6,
+      message: "최소 6자입니다."
     }
   }
 
@@ -55,7 +56,10 @@ const Form = ({title}) => {
 
       </div>
       <button type='submit'>{title}</button>      
-      <span className={styles.form_error}></span>
+      {
+        firebaseError &&
+        <span className={styles.form_error}>{firebaseError}</span>
+      }      
     </form>
   )
 }
--- src/pages/RegisterPage/sign-up/SignUp.jsx
+++ src/pages/RegisterPage/sign-up/SignUp.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from '../../../components/form/Form'
+import { useNavigate } from 'react-router-dom'
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth'
+import app from '../../../firebase'
 
 const SignUp = () => {
+  const navigate = useNavigate();
+  const [firebaseError, setFirebaseError] = useState("");
+
+  const auth = getAuth(app);
+  const handleSignUpAndLogin = (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((user) => {
+      // 리덕스 스토어에 담는 부분
+      navigate('/');
+    }).catch((error) => {
+      return error && setFirebaseError("이메일 또는 비밀번호가 잘못되었습니다.")
+    })
+  }
   return (
     <Form 
       title={"가입하기"} 
+      getDataForm={handleSignUpAndLogin}
+      firebaseError={firebaseError}
     />
   )
 }
