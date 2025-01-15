b1dfd3c2a3c28870b194d34fd4086c9f3cbc51f7
ljs981026 <ljs981026@naver.com>
2025-01-15 23:15:53 +0900

    상품 목록 노출

--- src/pages/HomePage/card-list/card-item/CardItem.jsx
+++ src/pages/HomePage/card-list/card-item/CardItem.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import styles from './CardItem.module.scss';
+import { Link } from 'react-router-dom';
+import { useAppSelector } from '../../../../hooks/redux';
 
-const CardItem = () => {
+const CardItem = ({ item }) => {
+  const {products} = useAppSelector(state => state.cart);
+  const productMatching = products.some((products) => products.id === item.id);
+
   return (
-    <div>CardItem</div>
+    <li className={styles.card_item}>
+      <Link to={`/product/${item.id}`}>
+        <img src={item.image} width={"80%"} height={"200px"} alt='product card' />
+      </Link>
+      <h5>{item.title.substring(0,15)}...</h5>
+      <div>
+        <button disabled={productMatching}>
+          {productMatching ? "장바구니에 담긴 제품" : "장바구니에 담기"}
+        </button>
+        <p>$ {item.price}</p>
+      </div>  
+    </li>
   )
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./user/user.slice";
 import categoriesSlice  from "./categories/categories.slice";
 import productsSlice  from "./products/products.slice";
+import cartSlice  from "./cart/cart.slice";
 
 export const store = configureStore({
   reducer: {
     user: userSlice, // 변경
     categories: categoriesSlice,
-    products: productsSlice
+    products: productsSlice,
+    cart: cartSlice
   },
 });
