843fb17c1f864b717349d48883f1339ab639ded9
ljs981026 <ljs981026@naver.com>
2025-01-16 22:18:47 +0900

    상품 상세페이지 추가

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./user/user.slice";
-import categoriesSlice  from "./categories/categories.slice";
-import productsSlice  from "./products/products.slice";
-import cartSlice  from "./cart/cart.slice";
+import categoriesSlice from "./categories/categories.slice";
+import productsSlice from "./products/products.slice";
+import cartSlice from "./cart/cart.slice";
+import productSlice from "./products/product.slice";
 
 export const store = configureStore({
   reducer: {
     user: userSlice, // 변경
     categories: categoriesSlice,
     products: productsSlice,
-    cart: cartSlice
+    cart: cartSlice,
+    product: productSlice
   },
 });
