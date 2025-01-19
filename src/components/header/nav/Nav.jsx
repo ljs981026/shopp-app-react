112e2b24821d2bd7fc129c0c14e9c7542444670a
ljs981026 <ljs981026@naver.com>
2025-01-19 22:00:30 +0900

    nav 컴포넌트 기능 추가

--- src/components/header/nav/Nav.jsx
+++ src/components/header/nav/Nav.jsx
@@ -3,17 +3,40 @@ import { FiLogIn, FiShoppingCart, FiUser } from 'react-icons/fi'
 import { GoSignOut } from 'react-icons/go'
 import { Link } from 'react-router-dom'
 import styles from './Nav.module.scss';
+import { useAuth } from '../../../hooks/useAuth';
+import { signOut, getAuth } from 'firebase/auth/cordova';
+import app from '../../../firebase';
+import { useAppDispatch, useAppSelector } from '../../../hooks/redux';
+import { removeUser } from '../../../store/user/user.slice';
+import { removeUserId } from '../../../store/cart/cart.slice';
 
 const Nav = () => {
+  const { isAuth } = useAuth(app);
+  const auth = getAuth();
+  const dispatch = useAppDispatch();
+  const { products } = useAppSelector((state) => state.cart);
+
+  const handleSignOut = () => {
+    signOut(auth)
+    .then(() => {
+      dispatch(removeUser());
+      dispatch(removeUserId());      
+    })
+    .catch(() => {
+
+    })
+  }
+  
   return (
     <nav className={styles.nav}>
       <ul>
         <li>
-          <div className={styles.couter}>
+          <div className={styles.counter}>
             <Link to={"/cart"}>
               {" "}
               <FiShoppingCart />
             </Link>
+            {products.length > 0 && <b>2</b>}
           </div>
         </li>
         <li>
@@ -21,15 +44,20 @@ const Nav = () => {
             <Link to={"/order"}>
               {" "}
               <FiUser title='주문'/>
-            </Link>
+            </Link>            
           </div>
         </li>  
         <li>
-          <GoSignOut className={styles.nav_sign_out} 
-          title='로그아웃'/>
-          <Link to={'/login'}>
-            <FiLogIn title='로그인' />
-          </Link>
+          {
+            isAuth ?
+            <GoSignOut className={styles.nav_sign_out} 
+            onClick={handleSignOut}
+            title='로그아웃'/>
+            :
+            <Link to={'/login'}>
+              <FiLogIn title='로그인' />
+            </Link>
+          }
         </li>                 
       </ul>
     </nav>
